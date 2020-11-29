3d81af5caf4305f1d697e6319b3b625e07292868
Thilanka <thilanka.nuwan89@gmail.com>
2020-11-29 18:08:04 +0530

    Keyboard input support

--- src/typeahead/TypeAhead.svelte
+++ src/typeahead/TypeAhead.svelte
@@ -1,94 +1,156 @@
-<script>
-    // import { slide } from "svelte/transition";
+<!-- 
+    @parameters:
 
-    export let items = [];
+items: [{value, text}] 
+
+TODO: Add item navigation by arrow keys?
+
+-->
+<script>
+
+    import { onMount } from 'svelte';
+    import { createEventDispatcher } from 'svelte';
+    
+	const dispatch = createEventDispatcher();
+    
+    export let items;
+    
+    let isLoading = !Array.isArray(items);
+    
+    $: _items = [];
+    
     let isVisible;
 
-    let userInput = "";
-    $: filtered = items
-        .sort()
-        .filter((itemVal) =>
-        userInput.length > 0 && itemVal.toLowerCase().startsWith(userInput.toLowerCase())
-        )
-        .map((itemVal) => {
-            let searchIndex = itemVal
-                .toLowerCase()
-                .indexOf(userInput.toLowerCase());
-            let obj = {};
-            obj.startingText = itemVal.substring(0, searchIndex);
-            obj.matchingText = itemVal.substring(
-                searchIndex,
-                searchIndex + userInput.length
-            );
-            obj.endText = itemVal.substring(
-                searchIndex + userInput.length,
-                itemVal.length
-            );
-            obj.text = itemVal;
-
-            return obj;
+    onMount(() => {
+        
+        Promise.resolve(items).then((val) => {
+            _items = val;
+            isLoading = false;
         });
-
-    items = [
-        "Papaya",
-        "Persimmon",
-        "Paw Paw",
-        "Prickly Pear",
-        "Peach",
-        "Pomegranate",
-        "Pineapple",
-    ];
-
-    function onSelect(v) {
-        userInput = v;
+        
+    });
+    
+    $: hoverIndex = isVisible?hoverIndex:-1;
+    let instantHide;
+    let itemSelected;
+    let ulElement; //<ul>
+        let userInput = "";
+        $: filtered = _items
+        .sort((a, b) => a.text.localeCompare(b.text))
+        .filter((itemVal) => {
+            return userInput.length > 0 &&
+            itemVal.text.toLowerCase().startsWith(userInput.toLowerCase());
+        })
+        .map((itemObj) => {
+            let itemTxt = itemObj.text;
+            let searchIndex = itemTxt
+            .toLowerCase()
+                .indexOf(userInput.toLowerCase());
+                let obj = {};
+                obj.startingText = itemTxt.substring(0, searchIndex);
+                obj.matchingText = itemTxt.substring(
+                    searchIndex,
+                    searchIndex + userInput.length
+                    );
+            obj.endText = itemTxt.substring(
+                searchIndex + userInput.length,
+                itemTxt.length
+                );
+                obj.text = itemTxt;
+                obj.value = itemObj.value;
+                
+                return obj;
+            });
+            
+        function onSelect(v) {
+            console.log(v);
+            
+            userInput = v.text;
+            instantHide = true;
+            isVisible = false; // It's better if this can be hidden without animation?
+            itemSelected = {text: v.text, value: v.value};
+        dispatch('selected', {
+            ...itemSelected
+        });
     }
 
     function keyUp(event) {
-
+        itemSelected = null;
         if (event.key === "Escape") {
             isVisible = false;
+        } else if (event.key === "ArrowDown") {
+            if(hoverIndex < filtered.length-1) {
+                arrowSelect("ArrowDown");
+                if(hoverIndex != filtered.length-1) {
+                    hoverIndex++;
+                }
+            }
+        } else if (event.key === "ArrowUp") {
+            if(hoverIndex > 0) {
+                arrowSelect("ArrowUp");
+                if(hoverIndex != 0) {
+                    hoverIndex--;
+                }
+            }
+        } else if (event.key === "Enter") {
+            // if(hov)
+            onSelect(filtered[hoverIndex]);
         } else if (filtered.length != 0) {
-            // document.getElementsByClassName('resultList')[0].style.display = 'block'
-            // document.getElementsByClassName('resultList')[0].style.height = '12em'
-            // document.getElementsByClassName('resultList')[0].style.opacity = 100
             isVisible = true;
         } else {
-            console.log("uuuuuuu");
             isVisible = false;
         }
     }
 
-    window.addEventListener("click", function (e) {
-        if (
-            !document.getElementById("sv-lib-type-container").contains(e.target)
-        ) {
-            // document.getElementsByClassName('resultList')[0].style.display = 'none'
-            isVisible = false;
-            // filtered = [];
-            // Clicked outside the search box
+    function arrowSelect(key) {
+        let listEles = ulElement.querySelectorAll('li');
+        let ul_Bounds = ulElement.getBoundingClientRect();
+        let li_Bounds;
+        if(key === "ArrowDown") {
+            if(hoverIndex >= 0) {
+                listEles[hoverIndex].classList.remove("li-hover");
+            }
+            listEles[hoverIndex+1].classList.add("li-hover");
+            
+            li_Bounds = listEles[hoverIndex+1].getBoundingClientRect()
+            
+            if((li_Bounds.top + li_Bounds.height) > (ul_Bounds.top + ul_Bounds.height)){ //Should scroll
+                
+                listEles[hoverIndex+1].scrollIntoView(false);
+            }
+            
+        } else if (key === "ArrowUp") {
+            if(hoverIndex != 0) {
+                listEles[hoverIndex].classList.remove("li-hover");
+            }
+            listEles[hoverIndex-1].classList.add("li-hover");
+            li_Bounds = listEles[hoverIndex-1].getBoundingClientRect()
+            if(li_Bounds.top < ul_Bounds.top){ //Should scroll
+                listEles[hoverIndex-1].scrollIntoView();
+            }
         }
-    });
+    }
 
-    document.addEventListener("keydown", function (event) {
-        const key = event.key; // Or const {key} = event; in ES6+
-        if (key === "Escape") {
-            // filtered = [];
-            // isVisible = false;
-        } else {
+    function windowClick(e) {
+        if (
+            e.target.id !== "_sv_lib_typahd_container" &&
+            e.target.id !== "_sv_lib_typahd_input" &&
+            e.target.id !== "_sv_lib_typahd_resultList_item"
+        ) {
+            if (!itemSelected) {
+                isVisible = false;
+                userInput = "";
+            }
         }
-    });
+    }
 </script>
 
 <style>
-    ul.resultList {
+    ul._sv-lib_resultList {
         display: block;
-        
+
         list-style: none;
-        /* width: 30rem; */
         width: 100%;
-        /* transition: width 0.3s; */
-        /* margin: auto; */
-        max-height: 12em;
         overflow: hidden;
         overflow-y: scroll;
         z-index: 100;
@@ -97,37 +159,34 @@
     }
 
     ul.hide {
-        /* height: 0em;
-        -webkit-transition: height, 0.2s linear;
-        -moz-transition: height, 0.2s linear;
-        -ms-transition: height, 0.2s linear;
-        -o-transition: height, 0.2s linear;
-        transition: height, 0.2s linear; */
         max-height: 0;
         transition: max-height 0.25s ease-out;
     }
 
+    ul.hide-fast {
+        max-height: 0;
+        transition: max-height 0.001s ease-out;
+    }
+
     ul.visible {
-        /* max-height: 500px;
-        -webkit-transition: height, 0.2s linear;
-        -moz-transition: height, 0.2s linear;
-        -ms-transition: height, 0.2s linear;
-        -o-transition: height, 0.2s linear;
-        transition: height, 0.2s linear; */
         max-height: 12em;
         transition: max-height 0.25s ease-in;
     }
 
-    ul.resultList li {
+    ul._sv-lib_resultList li {
         display: block;
         background: white;
-        /* margin: 10px auto; */
         padding: 10px;
         font-size: 1.2rem;
         border-radius: 2px;
     }
 
-    ul.resultList li:hover {
+    ul._sv-lib_resultList li:hover {
+        background: #eeeeee;
+        cursor: "context-menu";
+    }
+
+    ul._sv-lib_resultList :global(li.li-hover) { /* Svelte compiler removes as unused styles (happens in dynamic styling) */
         background: #eeeeee;
         cursor: "context-menu";
     }
@@ -137,19 +196,33 @@
     }
 </style>
 
-<div class="type-container" id="sv-lib-type-container">
-    <input type="text" bind:value={userInput} on:keyup={keyUp} class="input" />
-    <!-- {#if isVisible} -->
-    <!-- <ul class="resultList" transition:slide="{{ duration: 175 }}"> -->
-    <ul class="resultList" class:hide={!isVisible} class:visible={isVisible}>
+{hoverIndex}
+{JSON.stringify(itemSelected)}
+<div
+    class="control type-container"
+    id="_sv_lib_typahd_container"
+    class:is-loading={isLoading}>
+    <input
+        type="text"
+        bind:value={userInput}
+        on:keyup={keyUp}
+        class="input"
+        id="_sv_lib_typahd_input"
+        autocomplete="off" />
+    
+    <ul bind:this={ulElement}
+        class="_sv-lib_resultList"
+        id="_sv_lib_typahd_resultList"
+        class:hide={!isVisible && !instantHide}
+        class:hide-fast={!isVisible && instantHide}
+        class:visible={isVisible}>
         {#each filtered as item}
-            <li class="option" on:click={onSelect(item.text)}>
+            <li on:click={onSelect(item)} id="_sv_lib_typahd_resultList_item">
                 {item.startingText}<b>{item.matchingText}</b>{item.endText}
             </li>
         {/each}
     </ul>
-    <!-- {/if} -->
+
 </div>
-<br />
-<br />
-<!-- <label>xxxxxxxxxxxxxxxxxxx</label><input type="text" /> -->
+
+<svelte:window on:click={windowClick} />
--- stories/typeahead.stories.js
+++ stories/typeahead.stories.js
@@ -1,11 +1,75 @@
 import { TypeAhead } from "../src/index";
+import { action } from '@storybook/addon-actions';
+
+const items = [
+    "Papaya1",
+    "Papaya2",
+    "Papaya3",
+    "Persimmon",
+    "Paw Paw",
+    "Prickly Pear",
+    "Peach",
+    "Pomegranate",
+    "Pineapple",
+];
+
+function changeItems() {
+    
+    items = new Promise((resolve, reject)=> {
+        setTimeout(()=>resolve(itemz), 5000)
+        // resolve()
+    }
+    );
+    
+}
 
 export default {
     title: 'Typeahead',
     component: TypeAhead
 }
 
-export const basicTypeAhead = () => ({
+const itemz = [
+    {text:"P100", value: "pppppp100"},
+    {text:"P1", value: "pppppp1"},
+    {text:"P2kkkkkk", value: "pppppp2"},
+    {text:"P2", value: "pppppp2"},
+    {text:"P2yyyyyyy", value: "pppppp2"},
+    {text:"P2", value: "pppppp2"},
+    {text:"P212", value: "pppppp2"},
+    {text:"P2rt", value: "pppppp2"},
+    {text:"P2", value: "pppppp2"},
+];
+
+let promise = new Promise((resolve, reject)=> {
+    setTimeout(()=>resolve(itemz), 1000)
+    // resolve()
+}
+);
+
+// function selected(v) {
+//     console.log(v);
+// }
+
+const actionsData = {
+    selected: action('selected'),
+  };
+
+export const withArrayItems = () => ({
+
     title: 'TypeAhead',
-    Component: TypeAhead
+    Component: TypeAhead,
+    props: {items: itemz},
+    //  on: {
+    //      selected: {...actionsData}
+    //  }
+    on: {...actionsData}
+})
+
+export const withPromise = () => ({
+    title: 'TypeAhead',
+    Component: TypeAhead,
+    props: {items: promise},
+     on: {
+         selected: {...actionsData}
+     }
 })
