02f9b67e5365de1f7d08b514d299b6ab57ecae3d
Thilanka <thilanka.nuwan89@gmail.com>
2021-02-16 19:24:16 +0530

    Updates docs and bumps version 0.0.4

--- component-demos/FormTable/FormTableDemo.svelte
+++ component-demos/FormTable/FormTableDemo.svelte
@@ -39,6 +39,41 @@ import { FormTable, PopupMessage } from "../../src/index";
 
 </script>
 
+<pre>{`
+
+let config = { enableSearch: true, noDuplicates: { id: 'id'} }
+
+let data = {
+    headers: [
+        {id: 'id', label: '#'}, // id maps to object key in item
+        {id: 'sku', label: 'Item SKU'},
+        {id: 'qty', label: 'qty'},
+        {id: 'comment', label: 'comment'},
+        {id: 'price', label: 'Price'}, 
+        {id: 'discount', label: 'Discount'}, 
+        {id: 'total', label: 'Total'}
+    ],
+    items: []
+}
+
+let tableItems = [
+        { id: '1', sku: 'X001', qty: {type: 'number'}, comment: {type: 'text'}, price: 550.00, discount: 10, total: 550000},
+        { id: '2', sku: 'X002', qty: {type: 'number'}, comment: {type: 'text'}, price: 200.00, discount: 10, total: 550000},
+        { id: '3', sku: 'X003', qty: {type: 'number'}, comment: {type: 'text'}, price: 100.00, discount: 10, total: 550000},
+    ]
+
+    let promise = new Promise((resolve, reject)=> {
+        setTimeout(()=>resolve(tableItems.map(i => {return {text: i.sku, value: i}})), 1000)
+    })
+
+    let insertData = promise //This will be for TypeAhead input (can be a promise or an array of objects)
+
+<FormTable {config} {data} {insertData} on:duplicate={onDuplicateEntry}></FormTable>
+`
+
+
+}</pre>
+
 
 <FormTable {config} {data} {insertData} on:duplicate={onDuplicateEntry}></FormTable>
 <PopupMessage message_text="Duplicate entry!" type="error" {show_popup} />
