5645c44bec3c0bd52bdb2249216a280a97bda75a
Thilanka <thilanka.nuwan89@gmail.com>
2021-05-02 17:11:32 +0530

    Adds support for confimation messages in popup

--- src/popups/PopupMessage.svelte
+++ src/popups/PopupMessage.svelte
@@ -1,11 +1,74 @@
 <script>
+
     export let title = 'Error occurred'
     export let show_popup = false;
     export let message_text = '';
     export let type = 'info'
 
-    export const show = () => {
+    $: yesButtonText = type == 'confirm' ? 'Yes' : 'Ok'
+    let noButtonText = 'No';
+
+    let userCallback; // only for confirm
+
+    /**
+     * Show confirmation message box. Only last opened ui will be shown!
+     * 
+     * @param callback
+     * @param queryText
+     * @param boxTitle
+     */
+     export const showConfirm = (callback, queryText='Confirm operation?', boxTitle='Confirmation') => {
+        
+        message_text = queryText
+        title = boxTitle
+        type = 'confirm'
+        userCallback = callback
         show_popup = true
+    }
+
+    /**
+     * Show message box. Only last opened ui will be shown!
+     * @param infoText
+     * @param boxTitle
+     */
+    export const showInfo = (infoText, boxTitle='Info') => {
+        message_text = infoText
+        title = boxTitle
+        type = 'info'
+        show_popup = true
+    } 
+
+    /**
+     * Show success box. Only last opened ui will be shown!
+     * @param infoText
+     * @param boxTitle
+     */
+    export const showSuccess = (infoText, boxTitle='Success') => {
+        message_text = infoText
+        title = boxTitle
+        type = 'success'
+        show_popup = true
+    } 
+
+    /**
+     * Show success box. Only last opened ui will be shown!
+     * @param errorText
+     * @param boxTitle
+     */
+    export const showError = (errorText, boxTitle='Error') => {
+        message_text = errorText
+        title = boxTitle
+        type = 'error'
+        show_popup = true
+    } 
+
+    const onAction = (action) => {
+        if (action == 0){
+            if(type == 'confirm'){
+                userCallback()
+            }
+        }
+        show_popup = false
     }
     
 </script>
@@ -27,7 +90,10 @@
             <div class="column">
             <button class:is-info="{type == 'info'}" class:is-danger="{type == 'error'}" class:is-warning="{type == 'warning'}" 
                 class:is-success="{type == 'success'}" class="button is-outlined is-pulled-right"
-                    on:click={(e) => (show_popup = false)}>Ok</button>
+                    on:click={() => onAction(0)}>{yesButtonText}</button>
+            {#if type == 'confirm'}
+                <button class="button is-outlined is-pulled-right" on:click={() => onAction(1)}>{noButtonText}</button>
+            {/if}
             </div>
         </footer>
     </div>
